7bf7a94f656e0d864e9735a9ed939ba1c16a8bdf
Prashant Sinha <7372627+pkvsinha@users.noreply.github.com>
2025-07-16 23:26:44 +0530

    refactor: app module

--- app/src/main/java/com/prashantsinha/MainScreen.kt
+++ app/src/main/java/com/prashantsinha/MainScreen.kt
@@ -1,7 +1,5 @@
 package com.prashantsinha
 
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -10,24 +8,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.prashantsinha.blog.ArticlesScreen
-import com.prashantsinha.core.model.Profile
-import com.prashantsinha.feature.profile.ProfileScreen
-import com.prashantsinha.home.HomeScreen
+import com.prashantsinha.navigation.AppNavigation
 import com.prashantsinha.navigation.BottomNavItem
-import com.prashantsinha.projects.ProjectsScreen
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-    val navs = listOf<BottomNavItem>(
+    val items = listOf<BottomNavItem>(
         BottomNavItem.Home,
         BottomNavItem.Projects,
         BottomNavItem.Articles,
@@ -39,7 +30,7 @@ fun MainScreen() {
             NavigationBar {
                 val navBackStack = navController.currentBackStackEntryAsState().value
                 val currentDestination = navBackStack?.destination
-                navs.forEach { screen ->
+                items.forEach { screen ->
                     NavigationBarItem(
                         icon = { Icon(screen.icon, contentDescription = screen.label) },
                         label = { Text(screen.label) },
@@ -56,33 +47,7 @@ fun MainScreen() {
             }
         }
     ) { innerPadding ->
-        NavHost(navController, startDestination = BottomNavItem.Home.route, Modifier.padding(innerPadding)) {
-            composable(BottomNavItem.Home.route) {
-                HomeScreen()
-            }
-            composable(BottomNavItem.Projects.route) {
-                ProjectsScreen()
-            }
-            composable(BottomNavItem.Articles.route) {
-                val context = LocalContext.current
-                ArticlesScreen (onPostClick = { url ->
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    context.startActivity(intent)
-                })
-            }
-            composable(BottomNavItem.Resume.route) {
-                // You can get your profile data from a ViewModel later
-                val myProfile = Profile(
-                    name = "Prashant Sinha",
-                    title = "Android Developer | Tech Enthusiast",
-                    bio = "Welcome to my portfolio! I build beautiful and functional Android apps with Kotlin and Jetpack Compose.",
-                    // Replace with a real image URL!
-                    profileImageUrl = "https://avatars.githubusercontent.com/u/1024025?v=4", // Example: Google's GitHub avatar
-                    skills = listOf("Kotlin", "Jetpack Compose", "Modular Architecture", "Android", "Git", "Coroutines")
-                )
-                ProfileScreen(profile = myProfile)
-            }
-        }
+        AppNavigation(navController, Modifier.padding(innerPadding))
     }
 
 }
--- app/src/main/java/com/prashantsinha/navigation/AppNavigation.kt
+++ app/src/main/java/com/prashantsinha/navigation/AppNavigation.kt
@@ -1,8 +1,48 @@
 package com.prashantsinha.navigation
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.prashantsinha.blog.ArticlesScreen
+import com.prashantsinha.core.model.Profile
+import com.prashantsinha.feature.profile.ProfileScreen
+import com.prashantsinha.home.HomeScreen
+import com.prashantsinha.projects.ProjectsScreen
 
 @Composable
-fun AppNavigation() {
-
+fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(navController, startDestination = BottomNavItem.Home.route, modifier = modifier) {
+        composable(BottomNavItem.Home.route) {
+            HomeScreen()
+        }
+        composable(BottomNavItem.Projects.route) {
+            ProjectsScreen()
+        }
+        composable(BottomNavItem.Articles.route) {
+            val context = LocalContext.current
+            ArticlesScreen (onPostClick = { url ->
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                context.startActivity(intent)
+            })
+        }
+        composable(BottomNavItem.Resume.route) {
+            // You can get your profile data from a ViewModel later
+            val myProfile = Profile(
+                name = "Prashant Sinha",
+                title = "Android Developer | Tech Enthusiast",
+                bio = "Welcome to my portfolio! I build beautiful and functional Android apps with Kotlin and Jetpack Compose.",
+                // Replace with a real image URL!
+                profileImageUrl = "https://avatars.githubusercontent.com/u/1024025?v=4", // Example: Google's GitHub avatar
+                skills = listOf("Kotlin", "Jetpack Compose", "Modular Architecture", "Android", "Git", "Coroutines")
+            )
+            ProfileScreen(profile = myProfile)
+        }
+    }
 }
